eaaf87116bb08b223dbdbed775fcde8b666f5152
Robert Quispe Huacho <82891418+RobertQH123@users.noreply.github.com>
2022-04-23 00:31:37 -0500

    autenticarse

--- apiRobert/auth.php
+++ apiRobert/auth.php
@@ -2,14 +2,22 @@
 require_once 'class/user.php';
 require_once 'class/token.php';
 $_user = new User;
-$_token = new Token;
+$_respuestas = new Respuestas;
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $postbody = file_get_contents("php://input");
     $postbody = json_decode($postbody);
     $result = $_user->loginUser($postbody);
-    $token = $_token->insertarToken($result);
-    print_r($token);
-}elseif($_SERVER["REQUEST_METHOD"] == "GET"){
-    echo "get";
+    header("Content-type: application/json");
+    if(isset($result["result"]["error_id"])){
+        $respons = $result["result"]["error_id"];
+        http_response_code($respons);
+    }else{
+        http_response_code(200);
+    }
+    echo json_encode($result);
+}else{
+    header("Content-type: application/json");
+    $respons = $_respuestas->error_405();
+    echo json_encode($respons);
 }
 ?>
