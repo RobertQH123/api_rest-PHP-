2169eab87d5a9521c6a2f721dda916c0366068e3
Robert Quispe Huacho <82891418+RobertQH123@users.noreply.github.com>
2022-04-23 00:30:46 -0500

    login usuario

--- apiRobert/class/user.php
+++ apiRobert/class/user.php
@@ -1,15 +1,36 @@
 <?php
 include_once "dataBase/db.php";
+include_once "class/respuestas.php";
 class User extends DB{
     public function loginUser($datos){
+        $_respuestas = new Respuestas;
+        $_token = new Token;
         $pass = md5($datos->password);
-        $query = $this->connect()->prepare("SELECT UsuarioId FROM usuarios WHERE Usuario = :usuario AND Password = :password");
+        $query = $this->connect()->prepare("SELECT UsuarioId,Estado FROM usuarios WHERE Usuario = :usuario AND Password = :password");
         $query->bindparam('usuario',$datos->usuario);
         $query->bindparam('password',$pass);
         $query->execute();
-        
-        $arr = $query->fetch(PDO::FETCH_OBJ)->UsuarioId;
-        return $arr;
+        if($query->rowCount()>0){
+            $arr = $query->fetchAll(PDO::FETCH_ASSOC);
+            $usuario = $arr[0]["UsuarioId"];
+            $estado = $arr[0]["Estado"];
+            if($estado == "Activo"){
+                $tokenre = $_token->insertarToken($usuario );
+                if($tokenre){
+                    $resul = $_respuestas->response;
+                    $resul["result"] = array(
+                        "token" => $tokenre
+                    );
+                    return $resul;
+                }else{
+                    return $_respuestas->error_500("error interno, no emos podido guardar");
+                }
+            }else{
+                    return $_respuestas->error_200("usuario inactivo");
+            }
+        }else{
+            return $_respuestas->error_200("datos incorrectos");
+        }
     }
 }
 ?>
